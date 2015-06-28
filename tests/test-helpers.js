65e7b42302f79a52d36b77875698cd1a33871d7f
David Yahalomi <davidyaha@gmail.com>
2015-06-29 01:56:03 +0300

    (Tests) Added cleanup to the collection2 fields on tests teardown

--- tests/test-helpers.js
+++ tests/test-helpers.js
@@ -6,7 +6,12 @@ teardown = function(test) {
   Books._migrations.remove({});
   Books.remove({});
   Stores.remove({});
-  test.equal(Books._migrations.find().count(), 0);
-  test.equal(Books.find().count(), 0);
-  test.equal(Stores.find().count(), 0);
+
+  // Deletes collection2 fields
+  delete Books._c2;
+  delete Stores._c2;
+
+  test.equal(Books._migrations.find().count(), 0, "Migrations collection is empty");
+  test.equal(Books.find().count(), 0, "Books collection is empty");
+  test.equal(Stores.find().count(), 0, "Stores collection is empty");
 };
